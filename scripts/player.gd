dcc12111e8f901b3cfe25d229b355d84e5efe87d
Vincent Froelich <spelonisen@gmail.com>
2026-04-30 20:09:46 +0200

    add basic player with movement and jump

---
+++ scripts/player.gd
@@ -0,0 +1,23 @@
+extends CharacterBody2D
+
+const SPEED := 120.0
+const JUMP_VELOCITY := -250.0
+
+
+func _physics_process(delta: float) -> void:
+	# Gravity
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	# Jump
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Horizontal movement
+	var direction := Input.get_axis("ui_left", "ui_right")
+	if direction != 0.0:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0.0, SPEED)
+
+	move_and_slide()
